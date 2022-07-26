79745f2cc03baaebaf7fe0709bb8b9e8a7241bda
AnikKDev <anikdev2016@gmail.com>
2022-07-27 00:00:55 +0600

    added new branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+// src/App.js
+
+
+// import header component
+import Footer from "./components/Footer";
+// import footer component
+import Header from "./components/Header";
+
 function App() {
   return (
     <div>
-      <h1 className="text-center text-6xl">React with Tailwind CSS</h1>
+      <Header />
+      <div className="min-h-[60vh]"></div>
+      <Footer />
     </div>
   );
 }
